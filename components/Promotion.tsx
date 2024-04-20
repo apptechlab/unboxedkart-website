d82f34bba38ca528ad9beb69832414f099aa1f9b
bornakpaul <bornakpaul@gmail.com>
2024-04-21 01:14:17 +0530

    code refactored for store link

--- components/Promotion.tsx
+++ components/Promotion.tsx
@@ -1,29 +1,26 @@
 import React from 'react'
-import Button from './Button'
 import Image from 'next/image'
 
 const Promotion = () => {
+
   return (
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Download Unboxedkart now!</h2>
-          <p className="regular-16 text-gray-10">Available on iOS and Android</p>
+          <h2 className="bold-40 lg:bold-54 xl:max-w-[320px]">Shop as your
+          comfort</h2>
+          <p className="regular-16 text-gray-10">Shopping on unboxedkart are now so much easier, with the
+          world's top brands at your fingertips. Simply click and go to
+          discover the best finds in electronics more!</p>
           <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
-            <Button 
-              type="button"
-              title="App Store"
-              icon="apple.svg"
-              variant="btn_white"
-              full
-            />
-            <Button 
-              type="button"
-              title="Play Store"
-              icon="android.svg"
-              variant="btn_dark_green_outline"
-              full
-            />
+            <a className='flex h-12 rounded-full items-center justify-center font-medium px-4 lg:px-8 text-base md:text-lg lg:text-xl !leading-none bg-white text-blue-80 space-x-2 w-44 lg:w-52 transition-all duration-300 hover:scale-[1.02]' href="https://play.google.com/store/apps/details?id=com.unboxedkart&hl=en&gl=US">
+              <img className='pr-[8px]' loading='lazy' height={28} width={26} src="playstore.png" alt="download on playstore" decoding='async'/>
+              Play Store
+              </a>
+              <a className='flex h-12 rounded-full items-center justify-center font-medium px-4 lg:px-8 text-base md:text-lg lg:text-xl !leading-none bg-white text-blue-80 space-x-2 w-44 lg:w-52 transition-all duration-300 hover:scale-[1.02]' href="https://apps.apple.com/in/app/unboxedkart/id1550972969">
+              <img className='pr-[8px]' loading='lazy' height={28} width={26} src="appstore.png" alt="download on playstore" decoding='async'/>
+              App Store
+              </a>
           </div>
         </div>
 
