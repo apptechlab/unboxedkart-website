d639d6b8c74f809c9ebe3e82c30b969a7d18dc61
bornakpaul <bornakpaul@gmail.com>
2024-05-01 12:41:50 +0530

    code refactored

--- components/LandingScreen.tsx
+++ components/LandingScreen.tsx
@@ -8,10 +8,10 @@ const LandingScreen = () => {
      <div className='overflow-hidden relative lg:h-[1200px] text-white flex justify-center  h-[400px]'>
           <div className='lg:flex lg:justify-center pb-10 overflow-x-hidden'>
                <div className='flex flex-col items-center w-full lg:items-center lg:flex-row lg:justify-between lg:max-w-7xl px-4 lg:px-8'>
-               <div className='relative w-[325px] lg:w-[800px] lg:mr-8 mb-8 lg:mb-12 lg:mt-40'>
+               <div className='animate-slide-right relative w-[325px] lg:w-[800px] lg:mr-8 mb-8 lg:mb-12 lg:mt-40'>
                     {<Image src='certify_repair.svg' alt='cerify_repair' width={2240} height={2240} />}
                </div>
-               <div className='w-full flex flex-col items-center'>
+               <div className='animate-slide-right w-full flex flex-col items-center'>
                     <div className="text-left">
                     <h3 className='text-[12px] lg:text-[20px] font-light leading-none lg:block mr-2 text-textPrimary text-left lg:text-left'>
                          Premium Products. Affordable Prices
@@ -24,7 +24,7 @@ const LandingScreen = () => {
                </div>
            </div>  
      </div>
-     <div className="w-[325px] lg:w-[1200px] flex flex-col bg-yellow-300 rounded-lg pb-8 pl-12 mb-28 items-center">
+     <div className="animate-slide-right w-[325px] lg:w-[1200px] flex flex-col bg-yellow-300 rounded-lg pb-8 pl-12 mb-28 items-center">
           <div>
           <h2 className='text-[40px] lg:text-[120px] font-light leading-none lg:block mr-2 text-textPrimary mt-8'>
           <span className="font-bold">
@@ -42,10 +42,10 @@ const LandingScreen = () => {
           </div>
      </div>
      <div className="flex flex-col items-center w-full">
-          <h2 className="text-[36px] lg:text-[90px] font-light leading-none lg:block mr-2 text-textPrimary mt-8 text-white">
+          <h2 className="animate-slide-right text-[36px] lg:text-[90px] font-light leading-none lg:block mr-2 text-textPrimary mt-8 text-white">
                What our customer say..
           </h2>
-     <div className="flex flex-row space-x-12">
+     <div className="flex flex-row space-x-12 animate-slide-right">
      {REVIEWS.map((reviews, index) => 
      <div className="flex flex-col mb-12 mt-8">
      <div className="overflow-hidden relative lg:w-[350px] text-white flex flex-col items-center w-[200px] text-center">
