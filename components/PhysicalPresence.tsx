d254678323f24dc48b9628faf9fedf966444e351
Bornak Paul <bornak.p@gormalone.com>
2024-05-02 10:22:03 +0530

    #d2p

--- components/PhysicalPresence.tsx
+++ components/PhysicalPresence.tsx
@@ -5,12 +5,12 @@ const PhysicalPresence = () => {
   return (
     <div className='w-full flex flex-col items-center text-center pt-24 pb-24 text-blue-800'>
       <div>
-      <h2 className='text-[32px] lg:text-[54px] font-light leading-none lg:block mr-2 text-textPrimary'>
+      <h2 className='text-[32px] lg:text-[54px] font-bold leading-none lg:block mr-2 text-textPrimary'>
       OUR<br/>PHYSICAL PRESENCE
      </h2>
      <div className='lg:mx-0 h-[2px] mt-5 w-400 bg-blue-800'></div>
      </div>
-     <h2 className='pt-8 text-[18px] lg:text-[22px] font-bold leading-none lg:block mr-2 text-textPrimary'>
+     <h2 className='pt-8 text-[18px] lg:text-[22px] font-normal leading-none lg:block mr-2 text-textPrimary'>
      We setup physical stores across states to make shopping experience easier & safer for our customers
      </h2>
      <div className="flexCenter max-container relative w-full pt-16">
