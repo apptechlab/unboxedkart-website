e5207415dd61a3e8f0e9b7e50640a960a46c3f51
Bornak Paul <bornak.p@gormalone.com>
2024-05-02 10:20:02 +0530

    why buy from us section

--- components/WhyBuyFromUs.tsx
+++ components/WhyBuyFromUs.tsx
@@ -6,7 +6,7 @@ const Repair = () => {
     <section className="flexCenter flex-col">
       <div className="padding-container max-container w-full pb-24 pt-12">
         <Image src="gadgets.svg" alt="camp" width={80} height={80} />
-        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+        <div className="lg:flex lg:flex-wrap text-center lg:text-start justify-between gap-5 lg:gap-10">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[420px] text-blue-700">
             Why<br/>
             buy from us?
@@ -15,7 +15,7 @@ const Repair = () => {
       </div>
 
       <div className="flexCenter max-container relative w-full">
-      <div className="flex flex-row space-x-24">
+      <div className="flex flex-col lg:flex-row lg:space-x-24">
      {BUY_FROM_US.map((buyFromUs, index) => 
      <div className="flex flex-col mb-12 mt-8">
      <div className="overflow-hidden relative lg:w-[350px] text-white flex flex-col items-center w-[200px] text-center">
