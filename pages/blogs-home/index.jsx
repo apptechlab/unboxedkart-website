2f3b23779b246c353f064fd07837204b5ee97310
Bornak Paul <bornak.p@gormalone.com>
2024-06-12 01:17:40 +0530

    #d2p

--- pages/blogs-home/index.jsx
+++
@@ -1,9 +0,0 @@
-import BlogDetailsScreen from "/Users/bornakpaul/projects/unboxedkart-website/components/BlogsDetailsScreen";
-
-function BlogsHome() {
-     return (
-       <BlogDetailsScreen/>
-     );
-}
-
-export default BlogsHome
